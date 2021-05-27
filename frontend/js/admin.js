56fb68e236b1a9d22ea491763e613e430ec1de68
N0v0cain3 <nousernameidea0709@gmail.com>
2021-05-27 21:16:44 +0530

    fix :ambulance: admin view fix

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -2,27 +2,28 @@ function getquiz() {
     var jwt = localStorage.getItem('JWT_Token')
     console.log(jwt)
     var xh = new XMLHttpRequest();
-    xh.open("GET", "http://localhost:3000/quiz/view?id=60afab9285e4e0d50adce366", true)
+    xh.open("GET", "http://localhost:3000/quiz/view?id=60af86175a5fdb67969188e6", true)
     xh.setRequestHeader('Content-Type', 'application/json')
     xh.setRequestHeader('Authorization', jwt)
     xh.send()
     xh.onload = function () {
         if (this.status == 200) {
             var data = JSON.parse(this.responseText)
-            console.log(data.quiz.quiz)
-            const newdata = data.quiz.quiz
+            console.log(data)
+            console.log(data.quiz)
+            const newdata = data.quiz
             console.log(newdata.questions)
             for (var i = 0; i < newdata.questions.length; i++) {
                 
                 $('#questionContainer').append(`<div class="item">
                 <div class="question">
                     <p>${newdata.questions[i].title}</p>
-                    <div class ="options" id="options"> </div>
+                    <div class ="options" id="options${i}"> </div>
                 </div>
                
             </div>`)
-            for (var j = 0; j < 3; j++){
-                $('#options').append(`<p>${newdata.questions[i].options[j]}</p>`)
+            for (var j = 0; j <=3; j++){
+                $(`#options${i}`).append(`<p>${newdata.questions[i].options[j]}</p>`)
             }
 
             }
